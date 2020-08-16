9e3af5170f1fc6791f2e41afe78f4728e35f4da2
Ashish Karn <ashishkrkarn@gmail.com>
2020-08-16 18:51:06 +0530

    new css changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -25,10 +25,10 @@ function Home() {
 return (<>
 <Header/>
 <div className="content">
-    <div className="content-inner">
+    <div className="content-home">
     {content || error || <img src={logo} className="App-logo" alt="logo"/>}
     </div>
-<img src="https://scontent.fpat2-1.fna.fbcdn.net/v/t1.0-9/12122642_970444236346564_5157396284211546413_n.jpg?_nc_cat=108&_nc_sid=174925&_nc_ohc=z8iulIVIrx8AX8f1ZME&_nc_oc=AQnL5NAE7GVuXtz8ykP_UGc45fng9IC0oyW53lTrNOq5nkQRukGp0THuQfAtWurWQg8MMMnJOAOD5XT1mPXIzz_f&_nc_ht=scontent.fpat2-1.fna&oh=0907d14056b3e505435c21612c28cfab&oe=5F5D7339" alt="My Profile"/>
+
 </div>
 
 </>);
