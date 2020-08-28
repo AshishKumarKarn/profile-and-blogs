d943b3381a3064449523292061128a776457a7e8
Ashish Karn <ashishkrkarn@gmail.com>
2020-08-29 02:08:00 +0530

    new css changes

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -7,7 +7,19 @@ return (<div>
 <Header/>
 <div className="content">
 <div className="content-profile-1">
-This is profile component..coming soon!!
+<h2>Proﬁle</h2>
+<p>
+A skilled and results-oriented IT professional with good exposure to web development and extensive experience in
+ IT industry. Currently working for a US based educational domain client with Agile methodology.
+A total of 6+ years of experience as a full stack developer. Have worked in technologies like Java 7,8 & 11, J2EE, Servlets,
+Struts 1.x, Struts 2.x, Spring MVC, Spring Data, SpringBoot, Dropwizard, Javascript, React JS, Angular JS, Angular, HTML, CSS etc.
+Have good exposure to multi-threading and reactive programming. Love to play with Data structure and algorithms.
+Active on Coding Platforms like HackerEarth, Tech-Gig, etc with 98+ percentile.
+</p>
+<a href="https://www.hackerearth.com/@ashishkumkarn">HackerEarth</a>
+<br/>
+<a href="https://www.techgig.com/ashishkarn4">TechGig</a>
+
 </div>
 </div>
 </div>);
